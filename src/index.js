cadbbba338f31a916bdc87f6743ef1df6acfe86c
1iekim <mikeil98@mail.ru>
2021-03-14 19:54:07 +0300

    feat: add body of function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let brackets = [];
+  for (let brackVal of bracketsConfig) {
+    for (let brackValTo of brackVal) {
+    brackets.push(brackValTo);
+    }
+  }
+ // brackets = brackets.join(''); 13+ 7-
+  console.log(brackets);
+  console.log('str: ' + str);
+  let stack = [];
+  let caunt = 0;
+  for(let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket)
+   // console.log(`bracket: ${bracket} ; brackets: ${brackets} ; stack: ${stack};`);
+    if (bracketsIndex === -1){
+      continue;
+    }
+    if (bracket == '|') {caunt++;}
+    if(bracketsIndex % 2 == 0) {
+      stack.push(bracketsIndex + 1);
+    } else {
+      if(stack.pop() !== bracketsIndex) {
+        return false;
+      }
+    }
+  }
+  if (caunt != 0 && caunt%2 == 0) return true;
+  return stack.length == 0;
 }
