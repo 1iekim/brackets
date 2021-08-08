9322bd235d5f54215846b4e716851ff0eb66fd8b
1iekim <mikeil.akovity@gmail.com>
2021-08-08 11:39:54 +0300

    fix: fix solution

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  
-  let brackets = [];
-  for (let brackVal of bracketsConfig) {
-    for (let brackValTo of brackVal) {
-    brackets.push(brackValTo);
+  let newConfig = bracketsConfig.map(x => x.join(''));
+
+  for(let i = 0; i<newConfig.length; i++) {
+    if(str.includes(newConfig[i])) {
+      return check(str.split(newConfig[i]).join(''),bracketsConfig);
     }
   }
- // brackets = brackets.join(''); 13+ 7-
-  console.log(brackets);
-  console.log('str: ' + str);
-  let stack = [];
-  let caunt = 0;
-  for(let bracket of str) {
-    let bracketsIndex = brackets.indexOf(bracket)
-   // console.log(`bracket: ${bracket} ; brackets: ${brackets} ; stack: ${stack};`);
-    if (bracketsIndex === -1){
-      continue;
-    }
-    if (bracket == '|') {caunt++;}
-    if(bracketsIndex % 2 == 0) {
-      stack.push(bracketsIndex + 1);
-    } else {
-      if(stack.pop() !== bracketsIndex) {
-        return false;
-      }
-    }
-  }
-  if (caunt != 0 && caunt%2 == 0) return true;
-  return stack.length == 0;
+
+  return str.length == 0;
 }
